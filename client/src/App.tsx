c144d5eb3a44d1b0d08b44003e8c118b2d3fe874
Hokori <46038399+Hokori23@users.noreply.github.com>
2021-03-30 12:14:35 +0800

    Revert "chore: 配置脚手架: commit-lint & 换行符 & 格式化配置文件"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,15 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img alt="logo" className="App-logo" src={logo} />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          rel="noopener noreferrer"
           target="_blank"
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
